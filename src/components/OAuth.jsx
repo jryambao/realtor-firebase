b64e1e678090df429ad5695fdf1faef1efd11b23
ralph yambao <johnralph266@gmail.com>
2023-12-02 21:05:16 +0800

    test

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -5,11 +5,11 @@ export default function OAuth() {
   return (
     <div>
       <button
-        className="flex justify-center items-center active:bg-red-300 mt-6 w-full my-3 p-3 font-medium uppercase shadow-md rounded-md text-xl text-black border-gray-500 bg-white hover:bg-red-600  hover:text-white transition duration-150 ease-in-out"
+        className="flex justify-center items-center active:bg-red-300 mt-6 w-full my-3 p-3 uppercase shadow-md rounded-md text-xl text-white border-gray-500 bg-red-700 hover:bg-red-600  hover:text-white transition duration-150 ease-in-out"
         type="submit"
       >
-        <FcGoogle />
-        <span className="uppercase text-xl font-medium px-4">
+        <FcGoogle className="bg-white rounded-full" />
+        <span className="uppercase text-lg px-4">
           Continue With Google
         </span>
       </button>
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <div>ForgotPassword</div>
-  )
+    <>
+      <h1 className="font-bold text-3xl uppercase text-center">
+        Forgot Password
+      </h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 mx-auto max-w-6xl">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            className="w-full rounded-2xl"
+            src="https://images.unsplash.com/photo-1523217582562-09d0def993a6?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="img"
+          />
+        </div>
+        <div className="md:w-[67%] lg:w-[40%] w-full mx-12">
+          <form className="">
+            <input
+              className="transition ease-in-out w-full my-3 p-3 rounded-md text-xl text-gray-700 bg-white border-gray-300"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email"
+            />
+
+            <div className="flex text-sm sm:text-lg pr-3 mt-4 items-start justify-between whitespace-nowrap">
+              <p>
+                Don&apos;t have a account?
+                <Link
+                  className="text-red-700 font-bold hover:text-black transition duration-150 ease-in-out"
+                  to="/sign-up"
+                >
+                  {" "}
+                  Register
+                </Link>
+              </p>
+              <p>
+                <Link
+                  className="text-red-700 font-bold hover:text-black transition duration-150 ease-in-out"
+                  to="/sign-in"
+                >
+                  Sign In Instead
+                </Link>
+              </p>
+            </div>
+            <button
+              className="active:bg-blue-900 mt-6 w-full my-3 p-3 font-semibold uppercase shadow-md rounded-md text-xl text-white bg-blue-700 hover:bg-blue-800 transition duration-150 ease-in-out"
+              type="submit"
+            >
+              Send Reset Email
+            </button>
+            <div className="flex items-center my-2 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <span className="px-4 text-gray-500">
+                OR
+              </span>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ForgotPassword
+export default ForgotPassword;
