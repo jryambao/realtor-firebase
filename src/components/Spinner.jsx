78b451fc956576fc36e79a9abd7b40039293aa17
WHGROUP\jyambao <johnralph266@gmail.com>
2024-02-05 19:50:00 +0800

    test

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,7 @@ export default function Spinner() {
     <>
       <div className="z-50 bg-black bg-opacity-5 flex items-center justify-center fixed left-0 right-0 top-0 bottom-0">
         <div className="flex items-center justify-center h-screen">
-          <div className="border-gray-400 h-20 w-20 animate-spin rounded-full border-8 border-t-red-600" />
+          <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-red-600" />
         </div>
       </div>
     </>
